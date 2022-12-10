ab206688de822b9ce46a8007494eedf0ae079f31
bushio <kuromady@gmail.com>
2022-12-10 16:08:41 +0900

    add sumit initial pose

--- src/aichallenge_submit/sample_code_cpp/src/sample.cpp
+++ src/aichallenge_submit/sample_code_cpp/src/sample.cpp
@@ -3,6 +3,7 @@
 #include "autoware_auto_system_msgs/msg/autoware_state.hpp"
 #include "autoware_auto_vehicle_msgs/msg/engage.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include "std_msgs/msg/string.hpp"
 
 #include <cstdio>
@@ -20,6 +21,9 @@ public:
     check_pos_publisher = this->create_publisher<geometry_msgs::msg::PoseStamped>(
       "/planning/mission_planning/checkpoint", 1);
 
+    init_pos_publisher =
+      this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 1);
+
     // subscriber
     state_subscriber = this->create_subscription<autoware_auto_system_msgs::msg::AutowareState>(
       "/autoware/state", 1, std::bind(&SampleCode::stateCallback, this, std::placeholders::_1));
@@ -28,6 +32,8 @@ public:
     check_info_msg = setUpMsgCheckInfo();
     goal_info_msg = setUpMsgGoalInfo();
     engage_msg = setUpMsgEngage();
+
+    initial_info_msg = setUpMsgInitialInfo();
 
     // other
     stepCounter = 0;
@@ -41,8 +47,11 @@ private:
     // std::cout << state << std::endl;
 
     // ego vehicle base
+    if (state == 1 && stepCounter == 0) {
+      init_pos_publisher ->publish(initial_info_msg);
+      std::cout << "Publish the Initial Point Info" << std::endl;
     // Goal Pos Pub --> Check Point Pub --> Engage Pub
-
+    }
     //  Publish the Goal Point Info
     if (state == 2 && stepCounter == 0) {
       stepCounter++;
@@ -78,6 +87,29 @@ private:
     return msg;
   }
 
+  // setup msg(initial info) 2022/12/5
+  geometry_msgs::msg::PoseWithCovarianceStamped setUpMsgInitialInfo()
+  {
+    auto msg = geometry_msgs::msg::PoseWithCovarianceStamped();
+    msg.header.frame_id = "map";
+    msg.pose.pose.position.x = 81379.2109375;
+    msg.pose.pose.position.y = 49917.625;
+    msg.pose.pose.position.z = 0.0;
+    msg.pose.pose.orientation.x = 0.0;
+    msg.pose.pose.orientation.y = 0.0;
+    msg.pose.pose.orientation.z = 0.26281721767320854;
+    msg.pose.pose.orientation.w = 0.9648456405531992;
+    msg.pose.covariance= {
+        0.0225, 0.0,   0.0,   0.0,      0.0,      0.0,
+        0.0,   0.0225, 0.0,   0.0,      0.0,      0.0,
+        0.0,   0.0,   0.0225, 0.0,      0.0,      0.0,
+        0.0,   0.0,   0.0,   0.000625, 0.0,      0.0,
+        0.0,   0.0,   0.0,   0.0,      0.000625, 0.0,
+        0.0,   0.0,   0.0,   0.0,      0.0,      0.000625,
+    };
+    return msg;
+  }
+
   // setup msg(goal info)
   geometry_msgs::msg::PoseStamped setUpMsgGoalInfo()
   {
@@ -100,12 +132,12 @@ private:
     auto msg = geometry_msgs::msg::PoseStamped();
     msg.header.frame_id = "map";
     msg.pose.position.x = 81398.4375;
-    msg.pose.position.y = 50173.85546875;
+    msg.pose.position.y = 49917.625;
     msg.pose.position.z = 0.0;
     msg.pose.orientation.x = 0.0;
     msg.pose.orientation.y = 0.0;
-    msg.pose.orientation.z = 0.07906878863544824;
-    msg.pose.orientation.w = 0.9968691622593824;
+    msg.pose.orientation.z = 0.26281721767320854;
+    msg.pose.orientation.w = 0.9648456405531992;
     return msg;
   }
 
@@ -113,6 +145,7 @@ private:
   rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::Engage>::SharedPtr engage_publisher;
   rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pos_publisher;
   rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr check_pos_publisher;
+  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr init_pos_publisher;
 
   // subscriber
   rclcpp::Subscription<autoware_auto_system_msgs::msg::AutowareState>::SharedPtr state_subscriber;
@@ -121,6 +154,8 @@ private:
   geometry_msgs::msg::PoseStamped check_info_msg = geometry_msgs::msg::PoseStamped();
   geometry_msgs::msg::PoseStamped goal_info_msg = geometry_msgs::msg::PoseStamped();
   autoware_auto_vehicle_msgs::msg::Engage engage_msg = autoware_auto_vehicle_msgs::msg::Engage();
+
+  geometry_msgs::msg::PoseWithCovarianceStamped initial_info_msg = geometry_msgs::msg::PoseWithCovarianceStamped();
   // other
   int stepCounter;
   int state;
